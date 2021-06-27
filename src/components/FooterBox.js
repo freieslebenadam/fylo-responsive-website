c8df95c8eeaa2065fef3c2252555a7e9c87e2c4e
freieslebenadam <adam.fre@seznam.cz>
2021-06-27 16:11:09 +0200

    simple validation label

--- src/components/FooterBox.js
+++ src/components/FooterBox.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../style/FooterBox.scss'
 import Button from './Button'
 
 function FooterBox() {
+    const emailRE = /^[A-Za-z0-9]+(\.[A-Za-z0-9]*)?@[a-z]*\.[a-z0-9]{2,3}$/
+    const [email, setEmail] = useState("")
+    let valid = emailRE.test(email)
+    const validString = valid ? "" : "Please enter a valid email address."
+
     return (
         <div className="container2">
             <div className="footer-box">
@@ -13,8 +18,13 @@ function FooterBox() {
                         type="email"
                         name="email"
                         placeholder="email@example.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
-                    <Button>
+                    <label>
+                        <span style={{opacity: 0}}>|</span>{validString}
+                    </label>
+                    <Button type="button">
                         Get Started For Free
                     </Button>
                 </form>
